c5ffce5f93bf6f93585b827e0fb21f3198adf763
Thomas Lee <thomas.lee@uconn.edu>
2021-07-13 16:35:38 -0400

    detail modal transition

--- src/components/DetailModal.vue
+++ src/components/DetailModal.vue
@@ -1,13 +1,22 @@
 <template>
-  <div v-if="showModal" class="fixed top-0 left-0 overlay bg-white pa6">
-    <div class="h-25">
-        <button @click="$emit('close-modal')" class="b f3 bn w3 h3 fixed top-1 right-1">X</button>
+  <transition name="fade">
+    <div v-if="showModal" class="fixed top-0 left-0 overlay bg-white">
+      <div>
+        <button
+          @click="$emit('close-modal')"
+          class="b f3 bn w3 h3 fixed top-1 right-1"
+        >
+          X
+        </button>
+      </div>
+      <div class="pa4 flex flex-column items-center">
+        <h1 class="f-headline-ns">Detail Modal</h1>
+        <p v-for="n in 10" :key="n" class="ma3 pv3 w-30-ns">
+          {{ ipsum.generateParagraphs(1) }}
+        </p>
+      </div>
     </div>
-    <div class="h-75 pa4">
-      <h1 class="f-headline orange">Detail Modal</h1>
-      <p v-for="n in 10" :key="n" class="ma3 pa3">{{ ipsum.generateParagraphs(1) }}</p>
-    </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -16,7 +25,7 @@ export default {
   props: {
     showModal: {
       type: Boolean,
-      default: false
+      default: false,
     },
   },
   data() {
@@ -29,11 +38,17 @@ export default {
 
 <style lang="scss" scoped>
 .overlay {
-//   background-image: linear-gradient(transparent 50vh, #fff 0, #fff);
+  //   background-image: linear-gradient(transparent 50vh, #fff 0, #fff);
   height: 100%;
   width: 100%;
   overflow-y: scroll;
   //   transition: transform 0.8s cubic-bezier(0, 0.7, 0.4, 1),
   //     visibility 0s cubic-bezier(0, 0.7, 0.4, 1) 0.8s;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
