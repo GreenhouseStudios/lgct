ae5b6f58853a09b9e88b78d76eb49f42659e4c3f
Thomas Lee <thomas.lee@uconn.edu>
2021-07-26 15:07:38 -0400

    add gsap

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="section">
     <Nav></Nav>
-    <img :src="require('/src/assets/lgct-logo.png')" class="w-50-ns w-70 ma6-ns mv7" />
+    <img
+      :src="require('/src/assets/lgct-logo.png')"
+      id="hero"
+      class="w-50-ns w-70 ma6-ns mv7"
+    />
   </div>
 </template>
 
 <script>
-import Nav from './Nav'
+import Nav from "./Nav";
+import { gsap } from "gsap";
 export default {
-    components: {
-        Nav,
-    },
+  components: {
+    Nav,
+  },
+  mounted() {
+    gsap.from(".section", { opacity: 0, duration: 3, ease: "power2.out" });
+    gsap.from("nav", { x: 500, duration: 1, ease: "power2.out" });
+  },
 };
 </script>
 
