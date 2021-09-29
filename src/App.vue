da18e6715a53c502a5ea538335f6c09991f94be0
Tom Lee <thomas.lee@uconn.edu>
2021-09-29 09:54:54 -0400

    add breakpoint indicator for dev purposes

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-   <router-view></router-view>
+    <div v-if="$route.path !== '/'">
+      <div class="fixed top-0 right-2 hot-pink z-2 f1 serif pa2 dn db-l">
+        Large
+      </div>
+      <div class="fixed top-0 right-2 hot-pink z-2 f3 serif pa2 dn db-m">
+        Medium
+      </div>
+      <div class="fixed top-0 right-2 hot-pink z-2 f5 serif pa2 dn-l dn-m db">
+        Small
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  components: {
-  },
+  components: {},
   data() {
-    return {
-
-    };
+    return {};
   },
-  
 };
 </script>
 
@@ -30,54 +37,53 @@ export default {
 #ind {
   background-image: url(../src/assets/indigenous-peoples.png);
 }
-.section{
+.section {
   min-height: 100vh;
-  overflow: hidden;
 }
-router-link{
-    text-decoration: none;
+router-link {
+  text-decoration: none;
 }
-$lgct-white: #FEFCF5;
-$lgct-red: #AA4F37;
-$lgct-teal: #70B9B2;
-.bg-lgct-white{
-  background-color: $lgct-white,
+$lgct-white: #fefcf5;
+$lgct-red: #aa4f37;
+$lgct-teal: #70b9b2;
+.bg-lgct-white {
+  background-color: $lgct-white;
 }
-.bg-lgct-red{
-  background-color: #AA4F37,
+.bg-lgct-red {
+  background-color: #aa4f37;
 }
-.bg-lgct-blue{
-  color: #82b8b3,
+.bg-lgct-blue {
+  color: #82b8b3;
 }
-.bg-lgct-teal{
-  background-color: #54BAB1,
+.bg-lgct-teal {
+  background-color: #54bab1;
 }
-.bg-lgct-gold{
-  background-color: #EDC35B,
+.bg-lgct-gold {
+  background-color: #edc35b;
 }
-.bg-lgct-black{
-  background-color: #000,
+.bg-lgct-black {
+  background-color: #000;
 }
-.lgct-black{
+.lgct-black {
   color: #000;
 }
-.lgct-white{
-  color: #FEFCF5,
+.lgct-white {
+  color: #fefcf5;
 }
-.lgct-red{
-  color: #AA4F37,
+.lgct-red {
+  color: #aa4f37;
 }
-.lgct-blue{
-  color: #82b8b3,
+.lgct-blue {
+  color: #82b8b3;
 }
-.lgct-teal{
-  color: #54BAB1,
+.lgct-teal {
+  color: #54bab1;
 }
-.lgct-gold{
-  color: #EDC35B,
+.lgct-gold {
+  color: #edc35b;
 }
-:root{
-  --box-shadow-color: #54BAB1;
+:root {
+  --box-shadow-color: #54bab1;
 }
 #curved-corner-bottomleft,
 #curved-corner-bottomright,
@@ -119,15 +125,15 @@ $lgct-teal: #70B9B2;
   right: 0;
   box-shadow: 50px -50px 0 0 red;
 }
-a{
+a {
   text-decoration: none;
   color: inherit;
   display: inherit;
 }
-h1{
-  font-family: "FrankRuhlLibre"  !important;
+h1 {
+  font-family: "FrankRuhlLibre" !important;
 }
-p{
+p {
   font-family: "Karla" !important;
 }
 // @font-face {
