e87c3b5323ceeec9b19205e56e18f619f0ed8c62
Tom Lee <jcblee@gmail.com>
2021-10-08 15:26:43 -0400

    scroll auto for modal body

--- src/components/DetailModal.vue
+++ src/components/DetailModal.vue
@@ -1,19 +1,19 @@
 <template>
   <transition name="fade">
-    <div v-if="showModal && card" class="fixed overlay flex items-center justify-center pa4 z-2 ">
-      <div class="bg-white m3 br4 relative mw-50-l z-4" v-click-outside="handleClickOutside">
+    <div v-if="showModal && card" class="fixed overlay flex items-center justify-center pa4 z-2">
+      <div class="bg-white m3 br4 relative mw-50-l z-4 h-100 top-0 bottom-0" v-click-outside="handleClickOutside">
         <div class="f1 bn absolute top-1 right-1 br-100">
           <button @click="$emit('close-modal')" class="br-100 w3 h3 flex items-center pl2 bn bg-moon-gray">
             <!-- <i class="fa fa-times"></i> -->
             <i class="fa fa-chevron-left"></i>
           </button>
         </div>
-        <div class="pa4 flex flex-column items-start tl">
+        <div class="pa4 flex flex-column items-start tl h-100">
           <h1 class="f1 lgct-red">{{ card.heading }}</h1>
-          <div class="ma3 pv3 lh-copy f4">
+          <div class="ma3 pv3 lh-copy f4 h-80 overflow-auto bg-light-gray pa3 br4">
             <p
-              v-if="card.fullBody"
-              v-html="blocksToHTML({ blocks: card.fullBody })"
+              v-if="card.body"
+              v-html="blocksToHTML({ blocks: card.body })"
             ></p>
             <p v-else>
               {{ ipsum.generateParagraphs(1) }}
@@ -84,5 +84,8 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+a{
+  color: #70b9b2;
 }
 </style>
