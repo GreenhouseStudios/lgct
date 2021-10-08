c1e0e84486837673b9fe4f124f935c10e7340aad
Mackenzie Fox <54718299+mackenzie-fox@users.noreply.github.com>
2021-10-08 14:23:52 -0400

    HD Video Update

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -39,7 +39,7 @@
           <!-- <img class="mw-30" :src="require('/src/assets/map.png')" /> -->
           <!-- <div style="background-image: url('require')"></div> -->
           <video autoplay loop>
-            <source :src="require('../assets/ArcsWStatesDrawOnV2.mp4')" loop type="video/mp4">
+            <source :src="require('../assets/FinalAnimationHD.mp4')" loop type="video/mp4">
           </video>
         </div>
         <curved-border :color="'red'" :left="true"></curved-border>
@@ -129,6 +129,31 @@
     </full-page>
   </div>
 </template>
+
+<script>
+  var video = document.getElementById('video1'), fraction = 0.8;
+
+  function checkScroll() {
+      var x = video.offsetLeft, y = video.offsetTop, w = video.offsetWidth, h = video.offsetHeight, r = x + w, //right
+      b = y + h, //bottom
+      visibleX, visibleY, visible;
+
+      visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
+      visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
+
+      visible = visibleX * visibleY / (w * h);
+
+      if (visible > fraction) {
+          video.play();
+      } else {
+          video.pause();
+      }
+  }
+
+  checkScroll();
+  window.addEventListener('scroll', checkScroll, false);
+  window.addEventListener('resize', checkScroll, false);
+</script>
 
 <script>
 import ipsum from "../ipsum.js";
