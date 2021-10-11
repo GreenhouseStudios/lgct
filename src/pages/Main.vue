85d3dedcdb806d4f21ecb5e94cd40a7d9228802d
Thomas Lee <thomas.lee@uconn.edu>
2021-10-11 12:05:57 -0400

    fix dup

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -11,21 +11,7 @@
         <div class="flex flex-column items-center justify-center h-100">
           <stacked-card headColor="bg-lgct-red" mainColor="bg-moon-gray">
             <template v-slot:heading>The Objective of Land Grab CT</template>
-            In 1862, the United States government passed “an Act donating public
-            lands to the several states and territories which may provide
-            Colleges for the benefit of Agriculture and the Mechanic Arts,”
-            known as the Morrill Act. These public lands were established by the
-            systematic and violent dispossession of Indigenous people by the
-            United States government. The land tied to UConn’s Land Grant status
-            spans 12 states originally stewarded by Indigenous tribes. The
-            colleges established benefitted white citizens then and continue to
-            disproportionately benefit white citizens today. The University of
-            Connecticut became Connecticut’s Land Grant college in 1893. Upon
-            receiving land grant status by the state, the University of
-            Connecticut directly benefited from the 178190.04 acres of land and
-            the $135,000.84 ($3,598,150.99 today) raised from it through annual
-            payments to the university. The state of Connecticut paid
-            $12,763.01.
+            The Land Grab CT project was inspired by the Land Grab U project, which extensively collected and mapped land data tied to land-grant universities and the 1862 Morrill Act. Our website is a localization of the data from the Land Grab U project and an expansion of their work in a larger colonial context. Land Grab CT specifically focuses on UConn’s status as a land-grant university and details UConn’s acquisition and control of the land it currently resides on as well as the parcels of land from which it benefits.This project aims to inform viewers about UConn’s participation in the construction of colonial systems of higher education. It invites them to interrogate their assumptions about these systems and their impact on Native communities.
           </stacked-card>
         </div>
         <curved-border :color="'black'"></curved-border>
