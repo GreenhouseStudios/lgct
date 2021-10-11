3960265b89dfbf46f6b28d868cb61d52bb2b6bcf
Thomas Lee <thomas.lee@uconn.edu>
2021-10-11 09:22:48 -0400

    revised footer sect

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="section h6 bg-black white pa5">
-      <div class="h5"></div>
-    <div class="w-50-l center"><div class="mt5">
-      <h1 class="f1">Thank you for visiting</h1>
-      <h1 class="f1">Land Grab CT</h1></div>
-      <p class="f2 sans-serif mt6">Follow us on social media</p>
-      <div class="flex center justify-center">
+  <div class="section dark-gray flex flex-column justify-center">
+    <home-button></home-button>
+    <rounded-card :heading="'Thanks for visiting Land Grab CT'" id="card">
+      <div>
+        <p class="f2-ns f4 sans-serif mb0">Follow us on social media</p>
+      </div>
+      <div class="flex justify-start">
         <a href="https://twitter.com/LandGrabCT" class="ma3"
-          ><i class="fa fa-2x fa-twitter"></i
+          ><i class="fa fa-2x fa-twitter blue"></i
         ></a>
         <a href="https://www.instagram.com/landgrabct/?hl=en" class="ma3"
-          ><i class="fa fa-2x fa-instagram"></i
+          ><i class="fa fa-2x fa-instagram black"></i
         ></a>
       </div>
       <div>
         <a href="https://greenhousestudios.uconn.edu/"
           >https://greenhousestudios.uconn.edu/</a
         >
+        <h3>© 2021 Greenhouse Studios</h3>
       </div>
-      <h3>© 2021 Greenhouse Studios</h3>
-    </div>
+    </rounded-card>
   </div>
 </template>
 
 <script>
-export default {};
+import HomeButton from "./HomeButton.vue";
+import RoundedCard from "./RoundedCard.vue";
+
+export default {
+  components: { HomeButton, RoundedCard },
+};
 </script>
 
 <style lang="scss" scoped>
+#card{
+    height: 85vh;
+}
 </style>
