1bacb8114189fc3aea93622bc17d6dac9e9a2d1b
Thomas Lee <thomas.lee@uconn.edu>
2021-10-11 09:04:57 -0400

    add more to footer section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,31 @@
 <template>
-    <div class="h6 w-100 bg-black white pa5">
-        <h3>Copyright 2021 Greenhouse Studios</h3>
+  <div class="section h6 bg-black white pa5">
+      <div class="h5"></div>
+    <div class="w-50-l center"><div class="mt5">
+      <h1 class="f1">Thank you for visiting</h1>
+      <h1 class="f1">Land Grab CT</h1></div>
+      <p class="f2 sans-serif mt6">Follow us on social media</p>
+      <div class="flex center justify-center">
+        <a href="https://twitter.com/LandGrabCT" class="ma3"
+          ><i class="fa fa-2x fa-twitter"></i
+        ></a>
+        <a href="https://www.instagram.com/landgrabct/?hl=en" class="ma3"
+          ><i class="fa fa-2x fa-instagram"></i
+        ></a>
+      </div>
+      <div>
+        <a href="https://greenhousestudios.uconn.edu/"
+          >https://greenhousestudios.uconn.edu/</a
+        >
+      </div>
+      <h3>© 2021 Greenhouse Studios</h3>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
